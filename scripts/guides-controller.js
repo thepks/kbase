6fa72d21e533e4800aa78c571626c03abf38794d
Graham <thepksgp@gmail.com>
2015-02-18 22:06:59 +0000

    Changed to Bootstrap css

--- scripts/guides-controller.js
+++ scripts/guides-controller.js
@@ -2,22 +2,27 @@ guidesController = function() {
 	var guidesPage;
 	var initialised = false;
 	var manage = false;
+	var logged_on = false;
 	var url = '/kbase';
 	var searchUrl1 = '/kbase/_design/kb/_search/keywords?q=description:';
   var searchUrl2 = '%20OR%20title:';
   var searchUrl3 = '&include_docs=true';
+  var self;
 
 	return {
 		init : function(page) {
 			if (!initialised) {
 				guidesPage = page;
+				self = this;
+        $(guidesPage).find('#logoffEvt').hide();
 
         console.log('Initialising');
+        self.hideAll();
 
 				$(guidesPage).find('#homeEvt').click(function(evt) {
 				  console.log('In event');
 					evt.preventDefault();
-					$(guidesPage).find('#home').parent().siblings().children('div').hide();
+					self.hideAll();
 					if (manage) {
 					  manage = !manage;
 					}
@@ -25,6 +30,10 @@ guidesController = function() {
 
 				$(guidesPage).find('#manageEvt').click(function(evt) {
 				  console.log('In event manage');
+				  $(guidesPage).find('#home').hide();
+	        $(guidesPage).find('#about').hide();
+          $(guidesPage).find('#authentication').hide();
+
 				  manage = ! manage;
 					evt.preventDefault();
 					$(guidesPage).find('#manage').slideToggle("slow");
@@ -33,6 +42,81 @@ guidesController = function() {
 					$(guidesPage).find('#newSubmit').val('Save');
 
 				});
+
+
+       	$(guidesPage).find('#logonEvt').click(function(evt) {
+  				  console.log('In event logon');
+	  				evt.preventDefault();
+	  				self.hideAll();
+		  			$(guidesPage).find('#authentication').slideToggle("slow");
+
+  				});
+
+          $(guidesPage).find("#searchBtnEvt").click(function(evt) {
+            evt.preventDefault();
+            $(guidesPage).find('#authentication').hide();
+            self.search();
+          });
+
+        	$(guidesPage).find('#logoffEvt').click(function(evt) {
+  				  console.log('In event logoff');
+	  				evt.preventDefault();
+		  			$(guidesPage).find('#logonEvt').show();
+            $(guidesPage).find('#logoffEvt').hide();
+            logged_on = false;
+  				  $.ajax({
+  				    type: "DELETE",
+  				    url: "/_session",
+  	  			  success: function(data, status, jqXHR){
+  	  			    // Mod to add in setting the cookie
+                console.log("Data: " + data + "\nStatus: " + status);
+              },
+              error: function(data,status) {
+                console.log("Error! "+ data + "\nStatus: " + status)
+                }
+                });
+
+            self.hideAll();
+  				});
+
+  				$(guidesPage).find('#authButton').click(function(evt) {
+  				  console.log('In logon');
+  				  var usr = $(guidesPage).find('#username').val();
+  				  var pwd = $(guidesPage).find('#password').val();
+  				  var jname = [];
+  				  $.ajax({
+  				    type: "POST",
+  				    url: "/_session",
+  				    data: {
+    				    name : usr,
+    				    password : pwd
+  	  			  },
+  	  			  success: function(data, status, jqXHR){
+                console.log("Data: " + data + "\nStatus: " + status);
+                auth_token = jqXHR.getResponseHeader("AuthSession");
+    	       	  $(guidesPage).find('#authentication').hide();
+    		  			$(guidesPage).find('#logonEvt').hide();
+                $(guidesPage).find('#logoffEvt').show();
+
+                self.hideAll();
+                logged_on = true;
+              },
+              error: function(data,status) {
+                console.log("Error! "+ data + "\nStatus: " + status)
+
+  				      $(guidesPage).find('#auth-results').show();
+  			        $(guidesPage).find('#auth-results').text('Logon Failed! ' + status);
+
+    				    setTimeout(function(){
+    				      $(guidesPage).find('#auth-results').hide();
+    				    },2000);
+
+
+                }
+                });
+
+  				});
+
 
 
 				// Add the save event handler
@@ -88,50 +172,24 @@ guidesController = function() {
 				$(guidesPage).find('#aboutEvt').click(function(evt) {
 				  console.log('In event');
 					evt.preventDefault();
-					$(guidesPage).find('#about').slideToggle("slow");
+					self.hideAll();
+					$(guidesPage).find('#about').slideToggle("show");
 				});
 
         $(guidesPage).find('#search').on('keydown',(function(evt) {
           if (evt.keyCode == 13) {
-            console.log('In search '+ $(this).val());
             evt.preventDefault();
-
-            var u = searchUrl1+$(this).val()+searchUrl2+$(this).val()+searchUrl3;
-            console.log(u);
-
-            $(guidesPage).find('#resultList tbody').remove();
-            $(guidesPage).find('#results').removeClass('not');
-            $(guidesPage).find('#results').show();
-
-            $.get(u,function(data,status) {
-              console.log("Data " + data);
-              var d = JSON.parse(data);
-  		        var pd = []
-  		        for (var i=0; i<d.rows.length;i++){
-  		          pd.push(d.rows[i].doc);
-  		        }
-
-  			      $(guidesPage).find('#resultList').append($('#resultsRow').tmpl(pd));
-
-
-              if (manage) {
-  					    $(guidesPage).find('.editNav').slideToggle("fast");
-              }
-
-              updateEventHandler();
-
-				    }).fail(function(jqXHR, textStatus, errorThrown) {
-			        console.log('Error! ' + errorThrown);
-				    });
+            $(guidesPage).find('#authentication').hide();
+            self.search();
           }
         }));
 
 				initialised = true;
-
 			}
+		},
 
 
-  function updateEventHandler() {
+  updateEventHandler : function() {
 
         $(guidesPage).find('.resultDetail').click(function(evt) {
           var val=$(this).attr('value');
@@ -202,11 +260,53 @@ guidesController = function() {
 			    });
 
         });
-  }
+    },
 
-	function closeAll(page) {
+	closeAll : function (page) {
 	  console.log('In close all');
     $(page).find('#thumbs').addClass('not');
-	}
+	},
+  hideAll : function() {
+	   $(guidesPage).find('#home').hide();
+	   $(guidesPage).find('#about').hide();
+	   $(guidesPage).find('#manage').hide();
+	   $(guidesPage).find('#results').hide();
+	   $(guidesPage).find('#authentication').hide();
+	 },
 
-}}}();
+
+	 search : function () {
+
+	    var val = $(guidesPage).find('#search').val();
+	    var u = searchUrl1+val+searchUrl2+val+searchUrl3;
+      console.log(u);
+
+      $(guidesPage).find('#resultList tbody').remove();
+      $(guidesPage).find('#results').removeClass('not');
+      $(guidesPage).find('#results').show();
+
+      $.get(u,function(data,status) {
+        console.log("Data " + data);
+        var d = JSON.parse(data);
+        var pd = []
+        for (var i=0; i<d.rows.length;i++){
+          pd.push(d.rows[i].doc);
+        }
+
+        $(guidesPage).find('#resultList').append($('#resultsRow').tmpl(pd));
+
+
+        if (manage) {
+  		    $(guidesPage).find('.editNav').slideToggle("fast");
+        }
+
+        updateEventHandler();
+
+      }).fail(function(jqXHR, textStatus, errorThrown) {
+        console.log('Error! ' + errorThrown);
+      });
+
+	 }
+  }
+
+}();
